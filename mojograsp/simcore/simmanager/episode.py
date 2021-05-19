f6e7b64a4cbc230fb673a883b4608b4e4ed4ae39
Keeganfn <keegan.nave@gmail.com>
2021-05-18 17:13:10 -0700

    Outline for sim manager completed, now working on integration of pybullet

---
+++ mojograsp/simcore/simmanager/episode.py
@@ -0,0 +1,30 @@
+
+
+
+#Eventually filled with standard values
+class Episode():
+	
+	def __init__(self):
+		print("Episode initialized")
+		None
+	
+	def setup(self):
+		print("Episode setting up")
+		None
+	
+	def reset(self):
+		print("Episode reset")
+		None
+
+	def episode_pre_step(self):
+		print("Episode Pre step")
+		None
+
+	def episode_post_step(self):
+		print("Episode Post step")
+		None
+
+	def episode_complete(self):
+		print("Episode complete")
+		None
+
