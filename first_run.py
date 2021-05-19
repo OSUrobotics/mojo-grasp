4cffc599cc7bc4ba5fa9f8aa9494f64dc16a3672
JCampbell9 <campbjos@oregonstate.edu>
2021-05-19 13:53:42 -0700

    Update first_run.py

--- first_run.py
+++ first_run.py
@@ -1,8 +1,9 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import os
 import json
 import sys
+import glob
 
 
 def main():
@@ -22,6 +23,18 @@ def main():
 
         with open(f'{direct}/User_Info.json','w') as write_file:
             json.dump(temp_dect, write_file, indent=4)
+    
+
+    # direct_queue = f'{direct}/'
+    # direct_archive = 
+    
+    if os.path.isdir(f'{direct}/hand_models/hand_queue_json/') and os.path.isdir(f'{direct}/hand_models/hand_archive_json/'):
+        print('works')
+
+    # try:
+    #     os.path.isdir
+    # except FileNotFoundError:
+    #     pass
 
 
     
