d69703b23ea9a037c6996ba82c6440e6ec3432b8
Keeganfn <keegan.nave@gmail.com>
2021-05-25 23:38:17 -0700

    added rough outline of sensor class, added function in hand class to create sensor index, modified setup file to include pybullet dependency

--- mojograsp/simcore/simobject/hand.py
+++ mojograsp/simcore/simobject/hand.py
@@ -3,6 +3,7 @@ from . import objectbase
 from . import handgeometry
 
 from mojograsp.simcore.actuators import fullyactuated, underactuated
+from mojograsp.simcore.sensors import sensorbase
 
 class Hand(objectbase.ObjectBase):
 
@@ -24,9 +25,20 @@ class Hand(objectbase.ObjectBase):
 			self.actuation = underactuated.UnderActuated(self.joint_index, self.id)
 
 
-	#placeholder		
+	#rough draft of sensor index creation
 	def create_sensor_index(self):
 		print("creating sensors")
+		for i in range(p.getNumJoints(self.id)):
+			info = p.getJointInfo(self.id, i)
+			sensor_name = info[1].decode("ascii")
+			sensor_type = info[2]
+			sensor_id = info[0]
+			
+			#if any joints have name with substring sensor we create a sensor object and add it to the index
+			if(sensor_name.find("sensor") != -1):
+				self.sensor_index[sensor_name] = sensorbase.SensorBase(self.id, sensor_name) 	
+
+
 
 	#creates joint dictionary with style name: number
 	def create_joint_index(self):
@@ -36,7 +48,6 @@ class Hand(objectbase.ObjectBase):
 			joint_name = info[1].decode("ascii")
 			joint_type = info[2]
 			joint_id = info[0]
-
 			#filters out fixed joints to exclude sensors but will need better solution in the future
 			if(joint_type != 4):
 				self.joint_index[joint_name] = joint_id	
