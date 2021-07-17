b368026fa499496cda9c8e7de6df781ed86cc04d
John Morrow <cras@craswork>
2021-07-16 17:09:48 -0700

    started moving functions over to phasemanager, editing simmanager to be correctly wired for phasemanager. Not done yet

--- mojograsp/simcore/simmanager/phasemanager.py
+++ mojograsp/simcore/simmanager/phasemanager.py
@@ -1,10 +1,54 @@
-
+import time
+import pybullet as p
 
 class PhaseManager:
     # TODO: get functional shell down
-    def __init__(self, phase_dict):
-        self.phases = phase_dict
+    def __init__(self, phase_dict, starting_phase):
+        self.phases = phase_dict  # dictionary with phase objects in it
+        self.starting_phase = starting_phase
+        self.current_phase = None
 
     def run_phases(self):
-        pass
+        self.current_phase = self.starting_phase
+        exit_flag = False
+
+        # for every phase in the dictionary we step until the exit condition is met
+        while (exit_flag == False):  # TODO: running phases now goes into phasemanager
+            self.current_phase.setup()
+            step_count = 0
+
+            # while exit condition is not met call step
+            while (self.current_phase.phase_exit_condition(step_count) == False):
+                self.step()
+                step_count += 1
+
+            # after exit condition is met we get the next phase name and set current phase to the specified value
+            next_phase = self.current_phase.phase_complete()
+            if (next_phase != None):
+                try:
+                    self.current_phase = self.phases[next_phase]
+                except:
+                    exit_flag = True
+                    print("Error: Could not find next phase " + str(next_phase))
+            # if phase is none we break the for loop early
+            else:
+                exit_flag = True
+                break
+
+    def step(self):
+        # select action before episode step
+        action = self.current_phase.select_action()
+
+        # phase prestep and execute called
+        self.current_phase.pre_step()  # TODO: do we need this? What about user functions?
+        self.current_phase.execute_action()
+
+        # simulator timesteps equaling one episode timestep
+        for i in range(self.episode_timestep_length):  # TODO: pass this in through constructor
+            # Pybullet stepped
+            p.stepSimulation()
+            time.sleep(self.sim_timestep)
+
+        # phase post step called
+        self.current_phase.post_step()
 
