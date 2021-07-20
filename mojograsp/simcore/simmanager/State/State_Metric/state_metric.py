d306200f038b355f77ada3325514cfba2641c9e9
Asar5 <asara@oregonstate.edu>
2021-07-20 11:41:14 -0700

    Created state metric base class for pybullet

--- mojograsp/simcore/simmanager/State/State_Metric/state_metric.py
+++ mojograsp/simcore/simmanager/State/State_Metric/state_metric.py
@@ -14,39 +14,38 @@ import pybullet as p
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from mojograsp.simcore.datacollection.stats_tracker_base import *
 from collections import OrderedDict
+from mojograsp.simcore.simmanager.State.State_Metric.state_metric_base import StateMetricBase
 
 
-class StateMetric:
-    _sim = None
-
-    def __init__(self, data_structure, hand, obj):
-        self.hand = hand
-        self.object = obj
-        flag = True
-        print('intializing with data strucure:', data_structure)
-        try:
-            self.data = StatsTrackerArray(data_structure[0], data_structure[1])
-            print('stats tracker array initialized with min and max', self.data.allowable_min, self.data.allowable_max)
-        except TypeError:
-            self.data = StatsTrackerBase(data_structure[0], data_structure[1])
-            print('stats tracker base initialized with min and max', self.data.allowable_min, self.data.allowable_max)
-        except KeyError:
-            self.data = []
+class StateMetricPyBullet(StateMetricBase):
 
     def get_value(self):
         return self.data.value
 
-    @staticmethod
-    def get_geom_name(keys):
-        """
-        TODO: Create a mapping between JSON nams and joint indices, etc
-        :param keys:
-        :return:
-        """
-        print("KEYS: {}".format(keys))
 
-        return
+class StateMetricAngle(StateMetricPyBullet):
+    pass
 
-    def update(self):
-        print("Updating:".format())
-        return
+
+class StateMetricPosition(StateMetricPyBullet):
+    pass
+
+
+class StateMetricVector(StateMetricPyBullet):
+    pass
+
+
+class StateMetricRatio(StateMetricPyBullet):
+    pass
+
+
+class StateMetricDistance(StateMetricPyBullet):
+    pass
+
+
+class StateMetricDotProduct(StateMetricPyBullet):
+    pass
+
+
+if __name__ == '__main__':
+    pass
