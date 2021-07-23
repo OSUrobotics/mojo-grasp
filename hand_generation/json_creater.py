ab03181ed6caff64fec9f655332339450bdf9994
JCampbell9 <campbjos@oregonstate.edu>
2021-07-23 12:16:50 -0700

    made the json_creator more user friendly

--- hand_generation/json_creater.py
+++ hand_generation/json_creater.py
@@ -12,55 +12,55 @@ def test():
     hand = {
         "hand_name": name,
         "palm": {
-            "left_joint": input("palm left_joint:  "),
-            "right_joint": input("palm right_joint:  "),
+            "left_joint": input("palm left_joint (pin or spring):  "),
+            "right_joint": input("palm right_joint (pin or spring):  "),
             "stiffness": 0
         },
         "left_finger": {
-            "number_segments": int(input("left finger number of segments(default=3):  ")),
+            "number_segments": int(input("left finger number of segments(1, 2, or 3):  ")),
             "finger_length": float(input("total length of the left finger(default=0.12m):  ")),
-            "segment_names": list(input("list of seg names:  ").split(" ")),
-            "prox": {
-                "ratio": float(input("left prox ratio:  ")),
-                "joint_bottom": input("left prox bottom joint:  "),
-                "joint_top": input("left prox top joint:  ")
+            "segment_names": list(input("list of segment names(ex: proximal intermediate distal):  ").split(" ")),
+            "proximal": {
+                "ratio": float(input("left proximal ratio: \n(if finger only has one segment enter 0) \n ")),
+                "joint_bottom": input("left proximal bottom joint (pin or spring): \n(same as palm left_joint) \n  "),
+                "joint_top": input("left proximal top joint (pin or spring):  ")
             },
             "intermediate": {
-                "ratio": float(input("left intermediate ratio:  ")),
-                "joint_bottom": input("left intermediate bottom joint:  "),
-                "joint_top": input("left intermediate top joint:  ")
+                "ratio": float(input("left intermediate ratio:\n(if finger is one or two segments enter 0) \n  ")),
+                "joint_bottom": input("left intermediate bottom joint (pin or spring): \n(Same as right proximal top joint)\n(if finger is one or two segments enter NA) \n  "),
+                "joint_top": input("left intermediate top joint (pin or spring): \n(if finger is one or two segments enter NA) \n  ")
             },
             "distal": {
                 "ratio": float(input("left distal ratio:  ")),
-                "joint_bottom": input("left distal bottom joint:  "),
-                "ending": input("left distal ending:  "),
+                "joint_bottom": input("left distal bottom joint(pin or spring): \n(depending on # of segments either same as proximal or intermediate top joint)\n  "),
+                "ending": input("left distal ending (round):  "),
                 "sensors": {
-                    "num": int(input("number of sensors: ")),
-                    "type": list(input("list the types of sensors in order:  ").split(" "))
+                    "num": int(input("number of sensors (0-9): ")),
+                    "type": list(input("list the types of sensors in order: \n(as of now the name does not do anything but in the furute will be used for the type of sensor) \n ").split(" "))
                 }
             }
         },
         "right_finger": {
-            "number_segments": int(input("right finger number of segments(default=3):  ")),
+            "number_segments": int(input("right finger number of segments(1, 2, or 3):  ")),
             "finger_length": float(input("total length of the right finger(default=0.12m):  ")),
-            "segment_names": list(input("list of seg names:  ").split(" ")),
-            "prox": {
-                "ratio": float(input("right prox ratio:  ")),
-                "joint_bottom": input("right prox bottom joint:  "),
-                "joint_top": input("right prox top joint:  ")
+            "segment_names": list(input("list of segment names(ex: proximal intermediate distal):  ").split(" ")),
+            "proximal": {
+                "ratio": float(input("right proximal ratio: \n(If finger onle has one segment enter 0) \n  ")),
+                "joint_bottom": input("right proximal bottom joint (pin or spring): \n(same as palm right_joint) \n  "),
+                "joint_top": input("right proximal top joint:  ")
             },
             "intermediate": {
-                "ratio": float(input("right intermediate ratio:  ")),
-                "joint_bottom": input("right intermediate bottom joint:  "),
-                "joint_top": input("right intermediate top joint:  ")
+                "ratio": float(input("right intermediate ratio: \n(if finger is one or two segments enter 0) \n  ")),
+                "joint_bottom": input("right intermediate bottom joint (pin or spring): \n(Same as right proximal top joint) \n(if finger is one or two segments enter NA) \n  "),
+                "joint_top": input("right intermediate top joint (pin or spring): \n(if finger is one or two segments enter NA) \n  ")
             },
             "distal": {
                 "ratio": float(input("right distal ratio:  ")),
-                "joint_bottom": input("right distal bottom joint:  "),
-                "ending": input("right distal ending:  "),
+                "joint_bottom": input("right distal bottom joint(pin or spring): \n(depending on # of segments either same as proximal or intermediate top joint)\n  "),
+                "ending": input("right distal ending (round):  "),
                 "sensors": {
-                    "num": int(input("number of sensors:  ")),
-                    "type": list(input("list the types of sensors in order:  ").split(" "))
+                    "num": int(input("number of sensors (0-9):  ")),
+                    "type": list(input("list the types of sensors in order: \n(as of now the name does not do anything but in the furute will be used for the type of sensor) \n  ").split(" "))
                 }
             }
         },#in meters
