804714ea421cb7f535b54de412699a9f7531c9e8
Asar5 <asara@oregonstate.edu>
2021-07-23 15:00:00 -0700

    Wokring on state space class

--- mojograsp/simcore/simmanager/environment.py
+++ mojograsp/simcore/simmanager/environment.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 import time
 import pybullet as p
+from mojograsp.simcore.simmanager.environment_base import EnvironmentBase
 
 
-class Environment:
+class Environment(EnvironmentBase):
     """
     This class is used to reset the simulator's environment, execute actions, and step the simulator ahead
     """
 
-    def __init__(self, sleep=1. / 240., steps=1):
+    def __init__(self, action_class, sleep=1. / 240., steps=1, hand=None, objects=None):
+        super().__init__(action_class)
         self.sleep = sleep
         self.steps = steps
-        pass
+        self.hand = hand
+        self.objects = objects
 
     def reset(self):
         pass
 
-    def step(self, vals):
-        phase = vals[0]
-        state = vals[1]
-        reward = vals[2]
+    def step(self, phase):
         phase.execute_action(phase.action)
-        self.step_sim()
-        print("State:", state)
-        if state is not None:
-            state.update()
-            observation = state.get_observation()
+        self.step_sim(phase.action)
+        print("State:", phase.state)
+        if phase.state is not None:
+            phase.state.update()
+            observation = phase.state.get_observation()
         else:
             observation = None
         return observation, None, None, None
 
-    def step_sim(self):
+    def step_sim(self, action_profile):
         for i in range(self.steps):
             p.stepSimulation()
             time.sleep(self.sleep)
+
+    def get_hand_curr_joint_angles(self, keys=None):
+        return self.hand.get_joint_angles(keys)
+
+    def get_obj_curr_pose(self, key):
+        return self.objects[key].get_curr_pose(self.objects[key].id)
