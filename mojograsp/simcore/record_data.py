faaba3a9a94f4db492418699b03c6cbba0a1333c
Nigel Swenson <swensoni@oregonstate.edu>
2022-07-11 15:27:51 -0700

    got data frame working and switched to saving as pkl files

--- mojograsp/simcore/record_data.py
+++ mojograsp/simcore/record_data.py
@@ -8,6 +8,7 @@ import logging
 from mojograsp.simcore.state import State, StateDefault
 from mojograsp.simcore.action import Action, ActionDefault
 from mojograsp.simcore.reward import Reward, RewardDefault
+import pickle as pkl
 
 
 class RecordData(ABC):
@@ -157,3 +158,101 @@ class RecordDataJSON(RecordData):
             with open(file_path, 'w') as fout:
                 self.episodes = {"episode_list": self.episode_data}
                 json.dump(self.episodes, fout)
+
+class RecordDataPKL(RecordData):
+    """RecordDataJSON Class (Optional default class for recording data to json files)"""
+
+    def __init__(self, data_path: str = None, data_prefix: str = "episode", save_all=False, save_episode=True,
+                 state: State = StateDefault(), action: Action = ActionDefault(), reward: Reward = RewardDefault()):
+        """
+        Constructor takes in State, Action and Reward objects from the user and saves their outputs at every 
+        timestep. Optionally can save each episode individually, alltogether, or not at all. State, Reward and 
+        Action defaults are given if user does not define them (they will return empty dictionaries). 
+
+        :param data_path: Desired directory path for storing resulting json files.
+        :param data_prefix: Named prefix before each json file. Defaults to episode
+        :param save_all: Set whether or not you would like to save all episodes into one file (Default False).
+        :param save_episode: Set whether or not you would like to save each episodes into individual files (Default True).
+        :param state: State object defined by user, if none given then :func:`~mojograsp.simcore.state.StateDefault` will be used. 
+        :param action: Action object defined by user, if none given then :func:`~mojograsp.simcore.action.ActionDefault` will be used. 
+        :param reward: Reward object defined by user, if none given then :func:`~mojograsp.simcore.reward.RewardDefault` will be used. 
+        :type data_path: str
+        :type data_prefix: str
+        :type save_all: bool
+        :type save_episode: bool
+        :type state: :func:`~mojograsp.simcore.state.State` 
+        :type action: :func:`~mojograsp.simcore.action.Action` 
+        :type reward: :func:`~mojograsp.simcore.reward.Reward` 
+        """
+        if not data_path:
+            logging.warn("No data path provided")
+        self.data_path = data_path
+        self.data_prefix = data_prefix
+        self.save_all_flag = save_all
+        self.save_episode_flag = save_episode
+        self.state = state
+        self.action = action
+        self.reward = reward
+        self.timestep_num = 1
+        self.episode_num = 0
+        self.timesteps = []
+        self.episode_data = []
+        self.current_episode = None
+        self.episodes = {}
+
+    def record_timestep(self):
+        """
+        Method called by :func:`~mojograsp.simcore.sim_manager.SimManager` every timestep. Records the user defined state, 
+        action, and reward. Saves the result to a timestep dictionary which is later used to form an episode.
+        """
+        state_reward_dict = {}
+        timestep_dict = {"number": self.timestep_num}
+        if self.state:
+            timestep_dict["state"] = self.state.get_state()
+        if self.reward:
+            timestep_dict["reward"] = self.reward.get_reward()
+        if self.action:
+            timestep_dict["action"] = self.action.get_action()
+        self.timesteps.append(timestep_dict)
+        self.timestep_num += 1
+
+    def record_episode(self):
+        """
+        Method called by :func:`~mojograsp.simcore.sim_manager.SimManager` after every episode. Compiles all of the 
+        timestep dictionaries into a list and adds it to the episode dicionary. 
+        """
+        episode = {"number": self.episode_num+1}
+        episode["timestep_list"] = self.timesteps
+        self.current_episode = episode
+
+        if self.save_all_flag:
+            self.episode_data.append(episode)
+
+        self.timesteps = []
+        self.timestep_num = 1
+        self.episode_num += 1
+
+    def save_episode(self):
+        """
+        Method called by :func:`~mojograsp.simcore.sim_manager.SimManager` after every episode. Saves the most recent
+        episode dictionary to a pkl file. 
+        """
+        if self.save_episode_flag and self.data_path != None:
+            file_path = self.data_path + \
+                self.data_prefix + "_" + str(self.episode_num) + ".pkl"
+            print(file_path)
+            with open(file_path, 'wb') as fout:
+                pkl.dump(self.current_episode, fout)
+        self.current_episode = {}
+
+    def save_all(self):
+        """
+        Method called by :func:`~mojograsp.simcore.sim_manager.SimManager` after all episodes are completed. Saves all
+        episode dictionaries to a pkl file. 
+        """
+        if self.save_all_flag and self.data_path != None:
+            file_path = self.data_path + \
+                self.data_prefix + "_all.pkl"
+            with open(file_path, 'wb') as fout:
+                self.episodes = {"episode_list": self.episode_data}
+                pkl.dump(self.episodes, fout)
