556136cb3e0ebc7584de0163cb59f12ae7270651
nigelswenson <swensoni@oregonstate.edu>
2023-05-09 13:38:05 -0700

    updated config writer and runhpc for hpc work

--- config_rewriter.py
+++ config_rewriter.py
@@ -9,7 +9,7 @@ import json
 import os
 import pathlib
 
-folder_path = './demos/rl_demo/data/throw_test'
+folder_path = './demos/rl_demo/data/HPC_runs'
 
 overall_path = pathlib.Path(__file__).parent.resolve()
 resource_path = overall_path.joinpath('demos/rl_demo/resources')
@@ -22,12 +22,13 @@ for folder in subfolders:
     curr_folder = str(batch_run_folder.joinpath(folder))
     with open(curr_folder+'/experiment_config.json', 'r') as file:
         config = json.load(file)
-    high_level_path =  pathlib.Path(config['save_path']).parent.resolve().parent.resolve().parent.resolve().parent.resolve()
+    high_level_path =  pathlib.Path(config['save_path']).parent.resolve().parent.resolve().parent.resolve().parent.resolve().parent.resolve()
     high_level_path = str(high_level_path)
     print('hlp', high_level_path)
     print('overall folder', str(overall_path))
     for k in config.keys():
         if type(config[k]) == str:
+            print('old',k,config[k])
             config[k] = config[k].replace(high_level_path, str(overall_path))
             print('new',k,config[k])
     with open(curr_folder+'/experiment_config.json', 'w') as file:
