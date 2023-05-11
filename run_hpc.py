71c43906d997697eac1c48b93fa487659b829f14
nigelswenson <swensoni@oregonstate.edu>
2023-05-11 12:54:00 -0700

    updated config rewriter and runhpc

--- run_hpc.py
+++ run_hpc.py
@@ -184,9 +184,10 @@ def main(run_id):
     print(run_id)
     folder_names = ['0_ftp_control','1_ftp_no_contact','2_ftp_no_contact_velocity','3_ftp_velocity','4_ftp_velocity_x1',
                     '5_ftp_velocity_x2','6_ftp_velocity_x3','7_ftp_velocity_x4','8_ftp_rollout_10','9_ftp_rollout_1']
+    folder_names = ['0_joint_vel_her','1_finger_pos_her']
     
     overall_path = pathlib.Path(__file__).parent.resolve()
-    run_path = overall_path.joinpath('demos/rl_demo/data/HPC_runs')
+    run_path = overall_path.joinpath('demos/rl_demo/data/hpc2')
     final_path = run_path.joinpath(folder_names[run_id-1])
     print(str(final_path))
     run_pybullet(str(final_path) + '/experiment_config.json',runtype='run')
