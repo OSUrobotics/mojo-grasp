758cc94c09890eb581c57b39de22fd2b32006755
Nigel Swenson <nigel.swenson@gmail.com>
2023-10-19 13:07:39 -0700

    pushing before I try to optimize the code for speed

--- mojograsp/simcore/gym_run.py
+++ mojograsp/simcore/gym_run.py
@@ -392,8 +392,9 @@ def main():
     # run_pybullet(overall_path+'/demos/rl_demo/data/single_direction_updated_reward/backward_left/experiment_config.json',runtype='run')
     # run_pybullet(overall_path+'/demos/rl_demo/data/single_direction_updated_reward/backward_right/experiment_config.json',runtype='run')
 
-    for name in file_list:
-        run_pybullet(overall_path + '/demos/rl_demo/data/wedge_15/wedge_' + name + '/experiment_config.json', runtype='run')
+    # for name in file_list:
+    #     run_pybullet(overall_path + '/demos/rl_demo/data/wedge_15/wedge_' + name + '/experiment_config.json', runtype='run')
+    run_pybullet(overall_path + '/demos/rl_demo/data/profiling/wedge_' + file_list[0] + '/experiment_config.json', runtype='run')
 
     #NOTE WE MAY WANT TO UPDATE THE MAGNITUDE OF THE MAXIMUM MOVEMENT TO BE 1/8 THE SIZE THAT IT WAS TO MATCH THE PREVIOUS SETUP
 
