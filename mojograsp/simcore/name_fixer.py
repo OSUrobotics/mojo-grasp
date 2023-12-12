e8b8438a6cdc0bab8045490fc679a3f232738172
Nigel Swenson <nigel.swenson@gmail.com>
2023-12-12 10:35:58 -0800

    preping for merge

--- mojograsp/simcore/name_fixer.py
+++ mojograsp/simcore/name_fixer.py
@@ -2,7 +2,7 @@ import os
 import re
 
 
-filepath = '/home/mothra/mojo-grasp/demos/rl_demo/data/FTP_fullstate_A_rand/Test/'
+filepath = '/home/orochi/mojo/mojo-grasp/demos/rl_demo/data/JA_fullstate_A_rand/Test/'
 test_files = os.listdir(filepath)
 num_cores = 16
 for filename in test_files:
