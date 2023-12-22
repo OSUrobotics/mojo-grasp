62f9967040c52c37f06921cd98b4772a588c738d
nigel swenson <swensoni@oregonstate.edu>
2023-12-21 17:19:28 -0800

    makin folders

---
+++ demos/rl_demo/makefolder.py
@@ -0,0 +1,14 @@
+import os
+
+JAs = ['Full', 'Half']
+hand_params = ['Hand', 'NoHand']
+Action_space = ['FTP','JA']
+hands = ['BothInterp','PalmInterp','PalmExtrap','FingerExtrap','FingerInterp', 'Everything']
+
+
+for k1 in JAs:
+    for k2 in hand_params:
+        for k3 in Action_space:
+            for k4 in hands:
+                temp = '_'.join([k1,k2,k3,k4])
+                os.mkdir(temp)
