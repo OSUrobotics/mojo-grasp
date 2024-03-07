da374602cb458cf1a285a56bce28b510060865be
nigel swenson <swensoni@oregonstate.edu>
2024-03-07 14:30:52 -0800

    updated reward functions to work past 2pi

--- mojograsp/simcore/reward_functions.py
+++ mojograsp/simcore/reward_functions.py
@@ -90,9 +90,10 @@ def solo_rotation(reward_container, tholds):
     # make the current angle set between +/- pi then subtract the two
 
     obj_rotation = reward_container['object_orientation'][2]
-    obj_rotation = (obj_rotation + np.pi)%(np.pi*2)
-    obj_rotation = obj_rotation - np.pi
-    reward = -abs(reward_container['goal_orientation'] - obj_rotation)
+    thing1 = (obj_rotation-reward_container['goal_orientation'])%(np.pi*2)
+    thing2 = (reward_container['goal_orientation']-obj_rotation)%(np.pi*2)
+    rot_temp = min(thing1,thing2)
+    reward = -rot_temp
     return float(reward), False
 
 
@@ -101,9 +102,10 @@ def rotation(reward_container, tholds):
     # make the current angle set between +/- pi then subtract the two
     goal_dist = reward_container['distance_to_goal']/0.01 # divide to turn into cm
     obj_rotation = reward_container['object_orientation'][2]
-    obj_rotation = (obj_rotation + np.pi)%(np.pi*2)
-    obj_rotation = obj_rotation - np.pi
-    reward = -abs(reward_container['goal_orientation'] - obj_rotation) - goal_dist*tholds['DISTANCE_SCALING']
+    thing1 = (obj_rotation-reward_container['goal_orientation'])%(np.pi*2)
+    thing2 = (reward_container['goal_orientation']-obj_rotation)%(np.pi*2)
+    rot_temp = min(thing1,thing2)
+    reward = -rot_temp - goal_dist*tholds['DISTANCE_SCALING']
     return float(reward), False
 
 
@@ -112,19 +114,52 @@ def rotation_with_finger(reward_container, tholds):
     # make the current angle set between +/- pi then subtract the two
     goal_dist = reward_container['distance_to_goal']/0.01 # divide to turn into cm
     obj_rotation = reward_container['object_orientation'][2]
-    obj_rotation = (obj_rotation + np.pi)%(np.pi*2)
-    obj_rotation = obj_rotation - np.pi
+    thing1 = (obj_rotation-reward_container['goal_orientation'])%(np.pi*2)
+    thing2 = (reward_container['goal_orientation']-obj_rotation)%(np.pi*2)
+    rot_temp = min(thing1,thing2)
     ftemp = -max(reward_container['f1_dist'], reward_container['f2_dist']) * 100 
-    reward = -abs(reward_container['goal_orientation'] - obj_rotation) - goal_dist*tholds['DISTANCE_SCALING']  + ftemp*tholds['CONTACT_SCALING']
+    reward = -rot_temp - goal_dist*tholds['DISTANCE_SCALING']  + ftemp*tholds['CONTACT_SCALING']
     return float(reward), False
 
 def slide_and_rotate(reward_container, tholds):
     obj_rotation = reward_container['object_orientation'][2]
-    obj_rotation = (obj_rotation + np.pi)%(np.pi*2)
-    obj_rotation = obj_rotation - np.pi
-    rotation_temp = -abs(reward_container['goal_orientation'] - obj_rotation)/np.pi # divide by pi to make rotation reward -1 when we are at opposite side
+    thing1 = (obj_rotation-reward_container['goal_orientation'])%(np.pi*2)
+    thing2 = (reward_container['goal_orientation']-obj_rotation)%(np.pi*2)
+    rotation_temp = min(thing1,thing2)
     ftemp = -max(reward_container['f1_dist'], reward_container['f2_dist']) * 100 # 100 here to make ftemp = -1 when at 1 cm
     temp = -reward_container['distance_to_goal']/reward_container['start_dist'] # should scale this so that it is -1 at start 
     ftemp,temp = max(ftemp,-2), max(temp, -2)
-    tstep_reward = temp*tholds['DISTANCE_SCALING'] + ftemp*tholds['CONTACT_SCALING'] + rotation_temp
+    tstep_reward = temp*tholds['DISTANCE_SCALING'] + ftemp*tholds['CONTACT_SCALING'] - rotation_temp/np.pi
     return float(tstep_reward), False
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    goal_angs = np.linspace(-12.56,12.56,1000)
+    end_angs = np.linspace(-12.56,12.56,1000)
+    goal = 1
+    tholds = {'DISTANCE_SCALING':0.1, 'CONTACT_SCALING':0.2}
+    y = np.zeros(len(end_angs))
+    for i, a1 in enumerate(end_angs):
+        reward_container = {'distance_to_goal':0, 'f1_dist':0, 'f2_dist':0, 'object_orientation':[0,0,a1], 'goal_orientation':goal}
+        y[i],_ = rotation_with_finger(reward_container,tholds)
+
+    plt.plot(end_angs,y)
+    plt.title(f'reward for goal angle of {goal}')
+    plt.xlabel('ending angle')
+    plt.ylabel('reward')
+    plt.show()
+
+    # x,y = np.meshgrid(goal_angs,end_angs)
+    # z = np.zeros((len(goal_angs),len(end_angs)))
+    # tholds = {'DISTANCE_SCALING':0.1, 'CONTACT_SCALING':0.2}
+    # for i,a1 in enumerate(goal_angs):
+    #     for j,a2 in enumerate(end_angs):
+    #         reward_container = {'distance_to_goal':0, 'f1_dist':0, 'f2_dist':0, 'object_orientation':[0,0,a2], 'goal_orientation':a1}
+    #         r,_ = rotation_with_finger(reward_container, tholds)
+    #         z[i,j] = r
+    # fig,ax = plt.subplots(subplot_kw={"projection": "3d"})
+    # ax.scatter(x, y, z, linewidth=0, antialiased=False)
+    # plt.xlabel('goal angs')
+    # plt.ylabel('ending angs')
+    # plt.show()
