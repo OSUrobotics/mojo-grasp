abf76f4088799cb1c4050041bc5e6a5770914e60
nigel swenson <swensoni@oregonstate.edu>
2024-03-12 15:44:21 -0700

    cleaned up multiprocess gym run, made image collection faster, updated vizualizations

--- tests/gif_builder.py
+++ tests/gif_builder.py
@@ -65,4 +65,4 @@ def make_video(filepath):
     frame_one.save(filepath+"Episode.gif", format="GIF", append_images=frames,
                save_all=True, duration=15*8, loop=0)
 
-make_video('/home/mothra/mojo-grasp/demos/rl_demo/data/JA_region_10_1/')
+make_video('/home/mothra/mojo-grasp/demos/rl_demo/data/FTP_halfstate_A_rand/')
