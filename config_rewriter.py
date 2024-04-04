e7601fa65cac635ad5371c9a64ec42d63e30109f
nigelswenson <swensoni@oregonstate.edu>
2024-04-04 12:52:57 -0700

    addressed hpc challenges

--- config_rewriter.py
+++ config_rewriter.py
@@ -9,7 +9,7 @@ import json
 import os
 import pathlib
 
-folder_path = './demos/rl_demo/data/HPC Runs'
+folder_path = './demos/rl_demo/data/HPC_reward_scaling_search'
 
 overall_path = pathlib.Path(__file__).parent.resolve()
 resource_path = overall_path.joinpath('demos/rl_demo/resources')
