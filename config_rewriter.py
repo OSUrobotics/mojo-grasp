c3eb62224dc4218ac6a39c268753ee072f49b81b
nigelswenson <swensoni@oregonstate.edu>
2024-04-17 15:25:38 -0700

    prepping hpc for merge

--- config_rewriter.py
+++ config_rewriter.py
@@ -9,7 +9,7 @@ import json
 import os
 import pathlib
 
-folder_path = './demos/rl_demo/data/HPC_DR_testing'
+folder_path = './demos/rl_demo/data/HPC_slide_testing'
 
 overall_path = pathlib.Path(__file__).parent.resolve()
 resource_path = overall_path.joinpath('demos/rl_demo/resources')
