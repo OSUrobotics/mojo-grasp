b831eaa5e3c60acdefbf8de41c8648a7711489be
nigelswenson <swensoni@oregonstate.edu>
2024-04-22 11:44:44 -0700

    preping hpc side for merge

--- run_hpc_multi.py
+++ run_hpc_multi.py
@@ -8,13 +8,18 @@ Created on Thu Nov 30 12:35:23 2023
 
 import pathlib
 import sys
+print('path',sys.path)
+print('version',sys.version)
+
 from demos.rl_demo import multiprocess_gym_run
+#print('path',sys.path)
+#print('version',sys.version)
 
 def main(run_id):
     print(run_id)
-    folder_names = ['JA_R10_S1','JA_R10_S3'] #['JA_R0_S10','JA_R10_S1','JA_R10_S3','JA_R10_S10']
+    folder_names = ['FTP_S1','FTP_S2','FTP_S3','JA_S1','JA_S2','JA_S3']
     overall_path = pathlib.Path(__file__).parent.resolve()
-    run_path = overall_path.joinpath('demos/rl_demo/data/HPC_slide_testing')
+    run_path = overall_path.joinpath('demos/rl_demo/data/HPC_slide_all_randomizations')
     final_path = run_path.joinpath(folder_names[run_id-1])
     print(str(final_path))
     multiprocess_gym_run.main(str(final_path) + '/experiment_config.json')
