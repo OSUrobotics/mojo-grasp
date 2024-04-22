b5f01b13a254c932ffb55cba94816dcc84829674
nigelswenson <swensoni@oregonstate.edu>
2024-04-22 11:44:44 -0700

    preping hpc side for merge

--- vel_run.sh
+++ vel_run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH -J PPOSliderewardScale_2                        # name of job
+#SBATCH -J PPOSlideFullRandom2                        # name of job
 #SBATCH -p preempt                                  # name of partition or queue
-#SBATCH --array=2                     # how many tasks in the array
+#SBATCH --array=3-6                     # how many tasks in the array
 #SBATCH -o log/LOG-%a.out                 # name of error file for this submission script
 #SBATCH -e log/ERROR-%a.err                 # name of error file for this submission script
 #SBATCH --mem=10G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu 16
-#SBATCH -t 2-00:00:00
+#SBATCH -t 1-00:00:00
 # load any software environment module required for app (e.g. matlab, gcc, cuda)
 #module load software/version
-#module load python3/3.6.8
+module load python3/3.8
 source /nfs/hpc/share/swensoni/mojo-env/bin/activate
 python3 --version
-which python3
+which python3 -a
 
 # run my job (e.g. matlab, python)
-srun --export=ALL python3 run_hpc_multi.py $SLURM_ARRAY_TASK_ID 
+srun --export=ALL /nfs/hpc/share/swensoni/mojo-env/bin/python3 run_hpc_multi.py $SLURM_ARRAY_TASK_ID 
