a1d5242008a9cc81a97b99fbaf85c0967d802318
nigelswenson <swensoni@oregonstate.edu>
2024-05-27 12:13:55 -0700

    prepping hpc

--- n_run.sh
+++ n_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -J PPORotate                        # name of job
+#SBATCH -J PPOFull                        # name of job
 #SBATCH -p dgx2                                  # name of partition or queue
-#SBATCH --array=1-6                     # how many tasks in the array
+#SBATCH --array=1                     # how many tasks in the array
 #SBATCH -o log/LOG-%a.out                 # name of error file for this submission script
 #SBATCH -e log/ERROR-%a.err                 # name of error file for this submission script
 #SBATCH --mem=10G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu 16
-#SBATCH -t 2-00:00:00
+#SBATCH -t 4-00:00:00
 
 # load any software environment module required for app (e.g. matlab, gcc, cuda)
 #module load software/version
