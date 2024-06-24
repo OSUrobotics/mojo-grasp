0da2549801046a9dd5f8dda846c47a79f213839e
nigelswenson <swensoni@oregonstate.edu>
2024-06-24 15:39:38 -0700

    fixed hpc bug

--- run_hpc_multi.py
+++ run_hpc_multi.py
@@ -17,15 +17,15 @@ from demos.rl_demo import multiprocess_gym_run
 
 def main(run_id):
     print(run_id)
-    run_path_names = ['Full_continue','Rotation_continue']
-    folder_names = ['JA_S3_larger_space_new_weight_contact']
+    run_path_names = ['Rotation_continue']
+    folder_names = ['JA_S3_new_weight_same_space','JA_S3_large_space_long_train']
     overall_path = pathlib.Path(__file__).parent.resolve()
     run_path = overall_path.joinpath('demos/rl_demo/data/')
-    final_path = run_path.joinpath(run_path_names[run_id-1])
-    final_path = final_path.joinpath(folder_names[0])
+    final_path = run_path.joinpath(run_path_names[0])
+    final_path = final_path.joinpath(folder_names[run_id-1])
     print(str(final_path))
     multiprocess_gym_run.main(str(final_path) + '/experiment_config.json')
     
 if __name__ == '__main__':
     # print(sys.argv)
-    main(int(sys.argv[1]),num_cpu=int(sys.argv[2])
+    main(int(sys.argv[1]),num_cpu=int(sys.argv[2]))
