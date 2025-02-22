41af63f39807057b884cf672e38d953d46548ea9
nigel swenson <swensoni@oregonstate.edu>
2025-02-21 16:28:30 -0800

    added in a basic bar plotting script

--- demos/rl_demo/bar_plot.py
+++ demos/rl_demo/bar_plot.py
@@ -2,28 +2,38 @@ from mojograsp.simcore.data_gui_backend import *
 
 import matplotlib.pyplot as plt
 
-base_path = '/home/mothra/mojo-grasp/demos/rl_demo/data/NTestLayer/Dynamic'
-folders = [ base_path+'/square_A/',
-            base_path+'/square25_A/',
-            base_path+'/circle_A/',
-            base_path+'/circle25_A/',
-            base_path+'/triangle_A/',
-            base_path+'/triangle25_A/',
-            base_path+'/square_circle_A/',
-            base_path+'/pentagon_A/',
-            base_path+'/trapazoid_A/']
+base_path = '/home/mothra/mojo-grasp/demos/rl_demo/data/ReLu_Static'
+other_path = '/home/mothra/mojo-grasp/demos/rl_demo/data/Dynamic_2'
+folders = [ '/square_A/',
+            '/square25_A/',
+            '/circle_A/',
+            '/circle25_A/',
+            '/triangle_A/',
+            '/triangle25_A/',
+            '/square_circle_A/',
+            '/pentagon_A/',
+            '/trapazoid_A/']
 
 keys = ['square', 'square25','circle','circle25','triangle','triangle25','square_circle','pentagon','trapazoid']
 backend = PlotBackend()
 means = []
 stds = []
+means2 = []
+std2 = []
 for folder, k in zip(folders, keys):
-    data = backend.draw_scatter_end_magic(folder)
+    data = backend.draw_scatter_end_magic(base_path + folder)
     means.append(data[0])
     stds.append(data[1])
     backend.reset()
+    data = backend.draw_scatter_end_magic(other_path + folder)
+    means2.append(data[0])
+    std2.append(data[1])
+    backend.reset()
+
 backend.clear_axes()
 backend.ax.set_aspect('auto',adjustable='box')
 
-plt.bar(keys,means)
+plt.bar(range(len(keys)),means,width=0.5)
+plt.bar(np.array(range(len(keys)))+0.2,means2,width=0.5)
+plt.legend(['ReLu Static', 'New Dynamic'])
 plt.show()
